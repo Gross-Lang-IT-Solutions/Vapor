4644948db597e7bf0ae338cba6f373a235e7b107
SemirSljiivic <96631036+SemirSljiivic@users.noreply.github.com>
2023-03-20 11:32:18 +0100

    Restructured the folders.

---
+++ Vapor-frontend_dev/Details.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Vapor;
+using static Vapor.Start;
+
+namespace Vapor
+{
+    /// <summary>
+    /// Interaktionslogik für Details.xaml
+    /// </summary>
+    public partial class Details : Window
+    {
+        private Game game;
+
+        
+
+        public Details(string name, string publisher, DateTime installDate, TimeSpan playTime, DateTime lastPlayed, string category, uint ageRating, string path, bool favorite)
+        {
+            InitializeComponent();
+
+            lbl.Content = name;
+            lbl_Copy.Content = publisher;
+            lbl_Copy1.Content = installDate;
+            lbl_Copy2.Content = playTime;
+            lbl_Copy3.Content = lastPlayed;
+            lbl_Copy4.Content = category;
+            lbl_Copy6.Content = ageRating;
+            lbl_Copy7.Content = path;
+            lbl_Copy8.Content = favorite;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
---
+++ Vapor-frontend_dev/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+
+
+namespace Vapor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+             firstIndicator.Visibility = Visibility.Hidden;
+            thirdIndicator.Visibility = Visibility.Hidden;
+        }
+        private void ExecuteEnterLevel()
+        {
+            MessageBox.Show("cddddd");
+        }
+
+        public void PwCh()
+        {
+
+            firstIndicator.Visibility = Visibility.Hidden;
+            secondIndicator.Visibility = Visibility.Visible;
+            fourthIndicator.Visibility = Visibility.Hidden;
+            thirdIndicator.Visibility = Visibility.Visible;
+        }
+        public void EmCh()
+        {
+
+            firstIndicator.Visibility = Visibility.Visible;
+            secondIndicator.Visibility = Visibility.Hidden;
+            fourthIndicator.Visibility = Visibility.Visible;
+            thirdIndicator.Visibility = Visibility.Hidden;
+        }
+        private void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PwCh();
+        }
+
+        private void emailInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            EmCh();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+            Start start = new Start();
+            start.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Start start = new Start();
+            start.Show();
+            this.Close();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+            Start start = new Start();
+            start.Show();
+            this.Close();
+        }
+    }
+}
